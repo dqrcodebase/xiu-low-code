ab40780f5cce5af234b1942b4c3b26f6b83e4524
dengqr <852601818@qq.com>
2023-01-18 17:32:30 +0800

    feat: 在全局环境中引入样式mixins

--- .umirc.ts
+++ .umirc.ts
@@ -1,20 +1,36 @@
+const path = require('path');
+
 export default {
+  cssLoader: {
+    localsConvention: 'camelCase',
+  },
+  lessLoader: {
+    // 声明全局变量
+    modifyVars: {
+      hack: 'true; @import "~@/assets/styles/mixins.less";',
+    }
+  },
   routes: [
     {
-      path: '/design',
-      component: '@/layouts/EditLayout/EditLayout',
-      routes: [
-        { path: '', component: '@/pages/Design/Design' },
-      ],
+      path: '/',
+      redirect: '/index'
     },
     {
 
-      path: '/',
-      component: '@/layouts/BasicLayout/BasicLayout',
+      path: '/index',
+      component: '@/layouts/BasicLayout/index',
       routes: [
         { path: '/index', component: '@/pages/index/index' },
       ],
     },
+    {
+      path: '/design',
+      component: '@/layouts/EditLayout/index',
+      routes: [
+        { path: '/design', component: '@/pages/Design/index' },
+      ],
+    },
+
   ],
 }
 
--- src/pages/Design/Design.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const App: React.FC = () => (
-  <>
-    design
-  </>
-);
-export default App;
